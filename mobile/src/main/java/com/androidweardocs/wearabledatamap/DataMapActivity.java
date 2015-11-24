ec436e27bb73ecb020ea46145dc215e9b1e011c5
Michael Hahn <mike@telhill.com>
2015-11-24 15:18:20 -0800

    Remove node iterator from SendToDataLayerThread.

--- mobile/src/main/java/com/androidweardocs/wearabledatamap/DataMapActivity.java
+++ mobile/src/main/java/com/androidweardocs/wearabledatamap/DataMapActivity.java
@@ -1,6 +1,5 @@
 package com.androidweardocs.wearabledatamap;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.format.Time;
--- wear/src/main/java/com/androidweardocs/wearabledatamap/DataMapActivity.java
+++ wear/src/main/java/com/androidweardocs/wearabledatamap/DataMapActivity.java
@@ -1,6 +1,6 @@
 package com.androidweardocs.wearabledatamap;
 
-import android.app.Activity;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
